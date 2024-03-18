b2a333c1c34a04181dc26441f39b4d64cdd78321
ARASHFADAEE <mehrdadfadaee4@gmail.com>
2024-03-17 18:53:54 -0700

    fix bug

--- single.php
+++ single.php
@@ -59,7 +59,7 @@
             if (have_posts()) :
                 while (have_posts()) : the_post(); ?>
 
-                    ?>
+                    
                     <?php
                     if (get_field('is_single')) {
                     ?>
