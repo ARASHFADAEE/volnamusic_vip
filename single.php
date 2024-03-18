128f738570e07bd6f17f8128f965dc0fd293012e
ARASHFADAEE <mehrdadfadaee4@gmail.com>
2024-03-17 18:45:22 -0700

    fix bug song time in single track in album

--- single.php
+++ single.php
@@ -177,6 +177,8 @@
                                         <?php while (have_rows('album')) : the_row();
                                             $song_name = get_sub_field('song-name');
                                             $song_url = get_sub_field('song-url');
+                                            $song_time_album = get_sub_field('song-time');
+
                                             
                                             static $i=1;
 
@@ -204,7 +206,7 @@
                                                         <path d="M21,14a1,1,0,0,0-1,1v4a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V15a1,1,0,0,0-2,0v4a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V15A1,1,0,0,0,21,14Zm-9.71,1.71a1,1,0,0,0,.33.21.94.94,0,0,0,.76,0,1,1,0,0,0,.33-.21l4-4a1,1,0,0,0-1.42-1.42L13,12.59V3a1,1,0,0,0-2,0v9.59l-2.29-2.3a1,1,0,1,0-1.42,1.42Z"></path>
                                                     </svg>
                                                 </a>
-                                                <span class="single-item__time">3:06</span>
+                                                <span class="single-item__time"><?php echo $song_time_album ?></span>
                                             </li>
 
                                         <?php endwhile ?>
